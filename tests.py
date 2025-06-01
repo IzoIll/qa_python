46fc5cc8c2bcb1f509bcff696fe249e9ef4ca7b8
dmitr <dmitrii.kostin.1996@mail.ru>
2025-06-01 11:40:49 +0300

    new_commit

--- tests.py
+++ tests.py
@@ -19,6 +19,6 @@ class TestBooksCollector:
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
-
+1
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
